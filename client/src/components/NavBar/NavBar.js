b8af3e45a5029306be08875719a32c3da5c6f9bb
Alex Eesley <alexandra.eesley@gmail.com>
2020-11-18 19:49:25 -0600

    added navbar header

---
+++ client/src/components/NavBar/NavBar.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './NavBar.css';
+
+function NavBar() {
+  return (
+<nav>
+  <span className="app-navbar">EMPLOYEE DIRECTORY</span>
+  <p>Find an employee using the search function or sorting by name!</p>
+</nav>
+  );
+}
+
+export default NavBar;
